26bce8b0b90b2b68e47de45529994725f0eb5cfa
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-20 00:29:22 +0100

    Add Vector2D

---
+++ Include/Ishiko/Collections/Vector2D.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Collections/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_COLLECTIONS_VECTOR2D_H_
+#define _ISHIKO_COLLECTIONS_VECTOR2D_H_
+
+#endif
